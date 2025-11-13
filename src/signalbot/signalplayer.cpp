146b50bc68aafe2386868d33b992d53e3b7322d6
SomePineaple <njweb@tutanota.com>
2025-11-13 17:15:10 +0000

    start working on sending messages

--- src/signalbot/signalplayer.cpp
+++ src/signalbot/signalplayer.cpp
@@ -5,13 +5,80 @@
 #include "signalplayer.h"
 
 #include "nlohmann/json.hpp"
+#include <cstdlib>
+#include <random>
+#include <boost/asio.hpp>
+
+using json = nlohmann::json;
+namespace asio = boost::asio;
+
+int randomInt() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(1, 10000);
+
+    return distrib(gen);
+}
 
 namespace game::clients {
-    SignalPlayer::SignalPlayer(const std::string &number) : phoneNumber(number) {}
+    // TODO: Fix hardcoded signal-cli executable location
+    SignalPlayer::SignalPlayer(const std::string& playerNumber, const std::string& botNumber) : 
+        phoneNumber(playerNumber), signalCli(ctx, "/usr/bin/signal-cli", {"-a", botNumber, "jsonRpc"}) {
 
+        }
 
     bool SignalPlayer::runTurn(GameState* gs) {
         // TODO: Have the signal player basically just do the same thing as the human player, but getting responses from the signal bot
         return false;
+    }
+
+    void SignalPlayer::sendUserMessage(const std::string& message) {
+        int messageId = randomInt();
+        json req = {
+            {"jsonrpc", "2.0"},
+            {"method", "send"},
+            "params", {{"message", message}, {"recipient", phoneNumber}},
+            {"id", messageId}
+        };
+
+        asio::write(signalCli, asio::buffer(req.dump()));
+
+        // Wait for signal-cli to confirm the message was sent.
+        bool recievedResult = false;
+        while (!recievedResult) {
+            std::string response;
+            asio::read_until(signalCli, asio::dynamic_buffer(response), '\n');
+            auto res = json::parse(response);
+            if (res.contains("id") && res["id"] == messageId && res.contains("result")) {
+                auto result = res["result"]["results"][0]["type"];
+                if (!(result == "SUCCESS")) {
+                    printf("Failed to send message to user: got response %s\n", response.c_str());
+                    exit(1);
+                } else {
+                    recievedResult = true;
+                }
+            } else {
+                printf("got response that doesn't make sense: %s\n", response.c_str());
+            }
+        }
+    }
+
+    std::string SignalPlayer::recieveUserMessage() {
+        bool recievedMessage = false;
+        std::string message;
+        while (!recievedMessage) {
+            std::string in;
+            asio::read_until(signalCli, asio::dynamic_buffer(in), '\n');
+            auto res = json::parse(in);
+            if (res.contains("method") && res["method"] == "receive" && res["params"]["envelope"].contains("dataMessage")) {
+                auto message = res["params"]["envelope"]["dataMessage"];
+                if (message["message"].is_string()) {
+                    message = message["message"];
+                    recievedMessage = true;
+                }
+            }
+        }
+
+        return message;
     }
 }
--- src/signalbot/signalplayer.h
+++ src/signalbot/signalplayer.h
@@ -5,15 +5,21 @@
 #ifndef DUMMY_SIGNALPLAYER_H
 #define DUMMY_SIGNALPLAYER_H
 
+#include <boost/process.hpp>
+
 #include "../game/player.h"
 
 namespace game::clients {
     class SignalPlayer final : public Player {
         const std::string phoneNumber;
         void sendUserMessage(const std::string& message);
+        void sendGameState(const GameState* gs);
         std::string recieveUserMessage();
+        boost::process::popen signalCli;
+        boost::asio::io_context ctx;
     public:
-        explicit SignalPlayer(const std::string& number);
+        explicit SignalPlayer(const std::string& playerNumber, const std::string& botNumber);
+        void close();
         bool runTurn(GameState *gs) override;
         //std::shared_ptr<Player> clone() const override;
     };
